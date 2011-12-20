9197afcbd23698076d2c75b924bebf4f07e64a96
Alicia Díez <adiezbal@gmail.com>
2011-12-20 13:15:12 +0100

    Working on actors animations

--- rails/app/assets/javascripts/socialcheesecake/actor.js
+++ rails/app/assets/javascripts/socialcheesecake/actor.js
@@ -12,12 +12,14 @@ var socialCheesecake = socialCheesecake || {};
 		}
 		this.id = settings.id;
 		this.opacity = 1;
+		this._focused = false;
+		this._selected = false;
+		this._hidden = false;
 		this.parents = [];
 		if(settings.parent) this.parents.push(settings.parent);
 		
 		var actor = this;
-		var gridIdPrefix = this.getCheesecake().grid.divIdPrefix;
-		var actor_div = document.getElementById(gridIdPrefix+this.id);
+		var actor_div = actor.getDiv();
 		var mouseoverCallback = function(){
 			var sector;
 			actor.focus();
@@ -35,32 +37,33 @@ var socialCheesecake = socialCheesecake || {};
 				sector.eventHandler("mouseout");
 				actor.parents[subsector].eventHandler("mouseout");
 			}
-			console.log("mouseout");
 		}
 		actor_div.addEventListener("mouseover", mouseoverCallback, false);
 		actor_div.addEventListener("mouseout", mouseoutCallback, false);
 		actor_div.addEventListener("mousedown", function(){
 			var sector;
-			if( arguments.callee.activeActor){
+			if( actor.isSelected()){
 				// Deactivate actor
-				arguments.callee.activeActor = false;
+				actor._selected = false;
 				actor_div.addEventListener("mouseover", mouseoverCallback, false);
 				actor_div.addEventListener("mouseout", mouseoutCallback, false);
 			}else{
 				//Activate actor
-				arguments.callee.activeActor = true;
+				actor._selected = true;
 				actor_div.removeEventListener("mouseover", mouseoverCallback, false);
 				actor_div.removeEventListener("mouseout", mouseoutCallback, false);
 			}
 		});
 	}
 	
+	socialCheesecake.Actor.prototype.isSelected = function() {
+		return this._selected;
+	}
+	
 	socialCheesecake.Actor.prototype.focus = function() {
-		var cheesecake = this.getCheesecake();
-		var gridIdPrefix = cheesecake.grid.divIdPrefix;
-		var actor_id = this.id;
-		var actor_div = document.getElementById(gridIdPrefix+actor_id);
+		var actor_div = this.getDiv();
 		var newClass="";
+		this._focused = true;
 		if (actor_div.getAttribute("class")){
 			if (!(actor_div.getAttribute("class").match(/\sfocused/) )){
 				newClass = actor_div.getAttribute("class").concat(" focused");
@@ -73,36 +76,24 @@ var socialCheesecake = socialCheesecake || {};
 	}
 	
 	socialCheesecake.Actor.prototype.unfocus = function() {
-		var cheesecake = this.getCheesecake();
-		var gridIdPrefix = cheesecake.grid.divIdPrefix;
-		var actor_id = this.id;
-		var actor_div = document.getElementById(gridIdPrefix+actor_id);
+		var actor_div = this.getDiv();
 		var newClass="";
+		this._focused = false;
 		if (actor_div.getAttribute("class")){
 			newClass = actor_div.getAttribute("class").replace(/(^|\s)focused($|\s)/, "");
 			actor_div.setAttribute("class", newClass);
 		}	
 	}
 	
-	socialCheesecake.Actor.prototype.isFocused = function() {
-		var cheesecake = this.getCheesecake();
-		var gridIdPrefix = cheesecake.grid.divIdPrefix;
-		var actor_id = this.id;
-		var actor_div = document.getElementById(gridIdPrefix+actor_id);
-		var focused = false;
-		if ((actor_div.getAttribute("class")) && 
-				(actor_div.getAttribute("class").match(/(^|\s)focused($|\s)/))){
-			focused = true;
-		}	
-		return focused;
+	socialCheesecake.Actor.prototype.isFocused = function() {		var actor = this;
+		var gridIdPrefix = this.getCheesecake().grid.divIdPrefix;
+		return this._focused;
 	}
 	
 	socialCheesecake.Actor.prototype.hide = function() {
-		var cheesecake = this.getCheesecake();
-		var gridIdPrefix = cheesecake.grid.divIdPrefix;
-		var actor_id = this.id;
-		var actor_div = document.getElementById(gridIdPrefix+actor_id);
+		var actor_div = this.getDiv();
 		var newStyle=" display: none;";
+		this._hidden = true;
 		if (actor_div.getAttribute("style")){
 			if (actor_div.getAttribute("style").match(/display\s*:\s*[a-z]*;/)){
 				newStyle = actor_div.getAttribute("style").replace(/display\s*:\s*[a-z]*;/, "display: none;");			
@@ -114,20 +105,20 @@ var socialCheesecake = socialCheesecake || {};
 	}
 	
 	socialCheesecake.Actor.prototype.show = function() {
-		var cheesecake = this.getCheesecake();
-		var gridIdPrefix = cheesecake.grid.divIdPrefix;
-		var actor_id = this.id;
-		var actor_div = document.getElementById(gridIdPrefix+actor_id);
+		var actor_div = this.getDiv();
+		this._hidden = false;
 		if (actor_div.getAttribute("style")){
 			var newStyle = actor_div.getAttribute("style").replace(/display\s*:\s*none;/, "");
 			actor_div.setAttribute("style", newStyle);
 		}
 	}
 	
+	socialCheesecake.Actor.prototype.isHidden = function() {
+		return this._hidden;
+	}
+	
 	socialCheesecake.Actor.prototype.fadeOut = function() {
-		var gridIdPrefix = this.getCheesecake().grid.divIdPrefix;
-		var actor_id = this.id;
-		var actor_div = document.getElementById(gridIdPrefix+actor_id);
+		var actor_div = this.getDiv();
 		
 		if (actor_div.getAttribute("style")){
 			if (actor_div.getAttribute("style").match(/opacity\s*:\s*[a-zA-Z0-9]*;/)){
@@ -142,5 +133,13 @@ var socialCheesecake = socialCheesecake || {};
 	socialCheesecake.Actor.prototype.getCheesecake = function (){
 		return this.parents[0].parent.parent;
 	}
+	
+	socialCheesecake.Actor.prototype.getDiv = function () {
+		var gridIdPrefix = this.getCheesecake().grid.divIdPrefix;
+		var actor_id = this.id;
+		var actor_div = document.getElementById(gridIdPrefix+actor_id);
+		return actor_div;
+	}
+	
 })();  
   
